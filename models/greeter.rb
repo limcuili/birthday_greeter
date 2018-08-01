efdb91ca8b79e530a56c90e5e8a45ac4a0211fc8
limcuili <limcuili@hotmail.com>
2018-08-01 14:23:29 +0100

    Created Greeter class

--- models/greeter.rb
+++ models/greeter.rb
@@ -1,4 +1,18 @@
+require 'date'
+
 class Greeter
+
+  def initialize(date)
+    @date = date
+  end
+
+  def greet
+    if @date == Date.today
+      'Happy Birthday!'
+    else
+      "There are x days left to your birthday"
+    end
+  end
 
 =begin
 greet 'hb' if today is birthday
--- spec/greeter_spec.rb
+++ spec/greeter_spec.rb
@@ -2,4 +2,11 @@ require_relative '../models/greeter.rb'
 
 describe Greeter do
 
+  let(:greeter) { described_class.new(Date.new(2018,8,1)) }
+
+  it 'wishes you happy birthday if it is your birthday' do
+    allow(Date).to receive(:today).and_return(Date.new(2018,8,1))
+    expect(greeter.greet).to eq 'Happy Birthday!'
+  end
+
 end

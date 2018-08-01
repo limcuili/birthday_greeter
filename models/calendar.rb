6adb43931ddef948a5fee9275c2a6b49c2276770
limcuili <limcuili@hotmail.com>
2018-08-01 14:35:39 +0100

    Calculates one day to birthday

--- models/calendar.rb
+++ models/calendar.rb
@@ -1,6 +1,14 @@
 require 'date'
 
 class Calendar
+
+  def initialize(date)
+    @date = date
+  end
+
+  def days_left
+    return (Date.today - @date).to_i
+  end
 =begin
 here we will count the @days_left until a birthday
 or it will determine that today is the birthday
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../models/calendar.rb'
 
 describe Calendar do
+  let(:calendar) { described_class.new(Date.new(2018,7,31)) }
+
+  it 'calculates that it is one day to your birthday' do
+    allow(Date).to receive(:today).and_return(Date.new(2018,8,1))
+    expect(calendar.days_left).to eq 1
+  end
 
 end
